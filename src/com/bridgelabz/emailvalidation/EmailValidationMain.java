dd03412909d95e7bf3465b3b6d6e031698d014fb
Nithin <thenithinraj@gmail.com>
2021-09-07 18:53:07 +0530

    ADDED : Welcome message for the user

---
+++ src/com/bridgelabz/emailvalidation/EmailValidationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.emailvalidation;
+
+public class EmailValidationMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Email Validator !");
+
+	}
+
+}
