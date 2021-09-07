1493fdd3560fbb8f67e7bbd9825c54617574e65c
Nithin <thenithinraj@gmail.com>
2021-09-07 19:03:02 +0530

    ADDED : Validation check for the first mandatory part of email

--- src/com/bridgelabz/emailvalidation/EmailValidationMain.java
+++ src/com/bridgelabz/emailvalidation/EmailValidationMain.java
@@ -5,8 +5,25 @@ import java.util.regex.Pattern;
 
 public class EmailValidationMain {
 
+	public static boolean checkForValidEmailAddress(String emailAddress) {
+		boolean isValidEmailAddress;
+		String emailRegex = "^[a-zA-Z]{3}";
+		Pattern pattern = Pattern.compile(emailRegex);
+		Matcher matcher = pattern.matcher(emailAddress);
+
+		isValidEmailAddress = matcher.matches();
+		return isValidEmailAddress;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Validator !");
-		
+		String inputEmailAddress = "abc";
+
+		if (checkForValidEmailAddress(inputEmailAddress)) {
+			System.out.println(inputEmailAddress + " is valid.");
+		} else
+			System.out.println(inputEmailAddress + " is invalid");
+
+	}
 
 }
