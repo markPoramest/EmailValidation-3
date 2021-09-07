9a6bcc43f6518f3dc72df4400fb0c8e86d18981f
Nithin <thenithinraj@gmail.com>
2021-09-07 19:00:57 +0530

    ADDED : Required import statements

--- src/com/bridgelabz/emailvalidation/EmailValidationMain.java
+++ src/com/bridgelabz/emailvalidation/EmailValidationMain.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.emailvalidation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidationMain {
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Validator !");
-
-	}
+		
 
 }
