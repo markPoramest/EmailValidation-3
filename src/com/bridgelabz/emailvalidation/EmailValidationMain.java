c716662e3ab88e9bcc4c1d25b241d258912a0435
markza1998 <2411kjn@gmail.com>
2021-09-12 06:32:30 +0700

    add thai regular expression and "-"

--- src/com/bridgelabz/emailvalidation/EmailValidationMain.java
+++ src/com/bridgelabz/emailvalidation/EmailValidationMain.java
@@ -7,7 +7,7 @@ public class EmailValidationMain {
 
 	public static boolean checkForValidEmailAddress(String emailAddress) {
 		boolean isValidEmailAddress;
-		String emailRegex = "^[a-zA-Z]{1,}[\\.\\_\\-\\+]?[a-zA-Z]*[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]{2}$";
+		String emailRegex = "^[\\u0E00-\\u0E7Fa-zA-Z]{1,}[\\.\\_\\-\\+]?[\\u0E00-\\u0E7Fa-zA-Z]*[@]{1}[\\u0E00-\\u0E7Fa-z0-9-]+[.]{1}[\\u0E00-\\u0E7Fa-z]+[.]?[\\u0E00-\\u0E7Fa-z]{2}$";
 		Pattern pattern = Pattern.compile(emailRegex);
 		Matcher matcher = pattern.matcher(emailAddress);
 
@@ -23,7 +23,23 @@ public class EmailValidationMain {
 			System.out.println(inputEmailAddress + " is valid.");
 		} else
 			System.out.println(inputEmailAddress + " is invalid");
+		testThDomain();
 
+	}
+
+	public static void testThDomain(){
+		String[] thDomains = new String[4];
+		thDomains[0] = "email-test@universal-acceptance-test.international";
+		thDomains[1] = "email-test@universal-acceptance-test.icu";
+		thDomains[2] = "อีเมลทดสอบ@ยูเอทดสอบ.ไทย";
+		thDomains[3] = "อีเมลทดสอบ@ทีเอชนิค.องค์กร.ไทย";
+		for(String thDomain : thDomains){
+			if (checkForValidEmailAddress(thDomain)) {
+				System.out.println(thDomain + " is valid.");
+			} else
+				System.out.println(thDomain + " is invalid");
+
+		}
 	}
 
 }
